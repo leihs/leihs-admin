aa1a057f06d5a6ff7c0c13e5addb8902668cdae0
uvb <urs.vanbinsbergen@zhdk.ch>
2024-04-11 16:15:01 +0200

    WIP: cleanup resources/(root)

--- src/leihs/admin/resources/main.cljs
+++ src/leihs/admin/resources/main.cljs
@@ -3,23 +3,6 @@
 
 (defn page []
   [:article.admin.my-5
-   #_(when-let [user @current-user/state*]
-       [breadcrumbs/nav-component
-        @breadcrumbs/left*
-        [[breadcrumbs-audits/audits-li]
-         [breadcrumbs/buildings-li]
-         [breadcrumbs/groups-li]
-         [breadcrumbs-inventory/inventory-li]
-         [breadcrumbs/inventory-fields-li]
-         [breadcrumbs-inventory-pools/inventory-pools-li]
-         [breadcrumbs/mail-templates-li]
-         [breadcrumbs/rooms-li]
-         [settings-breadcrumbs/settings-li]
-         [breadcrumbs-statistics/statistics-li]
-         [breadcrumbs/suppliers-li]
-         [breadcrumbs-system/system-li]
-         [breadcrumbs-users/users-li]]])
-
    [:div
     [:h1 "Admin"]
     [:p "The application to administrate this instance of "
