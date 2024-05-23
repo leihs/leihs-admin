7c34374623d014e32c8252d581ee056b975d36d6
Matus Kmit <matuskmit1@gmail.com>
2024-05-23 15:03:43 +0200

    wip

--- src/leihs/admin/resources/inventory_pools/inventory_pool/workdays/main.cljs
+++ src/leihs/admin/resources/inventory_pools/inventory_pool/workdays/main.cljs
@@ -5,6 +5,7 @@
    [leihs.admin.resources.inventory-pools.inventory-pool.workdays.core :as core]
    [leihs.admin.resources.inventory-pools.inventory-pool.workdays.edit :as edit]
    [leihs.admin.utils.misc :refer [wait-component]]
+   [leihs.core.front.debug :refer [spy]]
    [leihs.core.routing.front :as routing]))
 
 (defn component []
@@ -16,12 +17,11 @@
      [:div
       [table/container
        {:borders false
-        :header [:tr [:th "Day"] [:th "Open/Closed"] [:th "Max. Allowed Visits"]]
+        :header [:tr [:th "Day"] [:th "Opened"] [:th "Max. Allowed Visits"]]
         :body (doall (for [day (keys core/DAYS)]
                        [:tr {:key day}
                         [:td (capitalize (name day))]
-                        [:td (if (@core/data* day) "Open" "Closed")]
+                        [:td (str (day @core/data*))]
                         [:td (or ((core/DAYS day) (@core/data* :max_visits))
                                  "unlimited")]]))}]
       [edit/button]])])
-
