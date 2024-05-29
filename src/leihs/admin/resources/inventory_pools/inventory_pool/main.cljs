c6c4153778fe4a00d0f68631dc34b9c4e6c59dc5
Matus Kmit <matuskmit1@gmail.com>
2024-05-29 14:04:03 +0200

    refactor

--- src/leihs/admin/resources/inventory_pools/inventory_pool/main.cljs
+++ src/leihs/admin/resources/inventory_pools/inventory_pool/main.cljs
@@ -4,7 +4,7 @@
    [goog.string :as string]
    [leihs.admin.common.components.table :as table]
    [leihs.admin.resources.inventory-pools.authorization :as pool-auth]
-   [leihs.admin.resources.inventory-pools.inventory-pool.core :as inventory-pool]
+   [leihs.admin.resources.inventory-pools.inventory-pool.core :as core]
    [leihs.admin.resources.inventory-pools.inventory-pool.delete :as delete]
    [leihs.admin.resources.inventory-pools.inventory-pool.edit :as edit]
    [leihs.admin.resources.inventory-pools.inventory-pool.nav :as nav]
@@ -16,32 +16,6 @@
 
 ;;; components ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defn edit-inventory-pool []
-  (when (auth/allowed? [pool-auth/pool-inventory-manager?
-                        auth/admin-scopes?])
-    (let [show (reagent/atom false)]
-      (fn []
-        [:<>
-         [:> Button
-          {:className ""
-           :onClick #(reset! show true)}
-          "Edit"]
-         [edit/dialog {:show @show
-                       :onHide #(reset! show false)}]]))))
-
-(defn delete-inventory-pool []
-  (when (auth/allowed? [auth/admin-scopes?])
-    (let [show (reagent/atom false)]
-      (fn []
-        [:<>
-         [:> Button
-          {:className "ml-3"
-           :variant "danger"
-           :onClick #(reset! show true)}
-          "Delete"]
-         [delete/dialog {:show @show
-                         :onHide #(reset! show false)}]]))))
-
 (defn property-td
   ([label col-name] (property-td label col-name nil))
   ([label col-name hint]
@@ -49,7 +23,7 @@
     (when hint [:small.form-text hint])]))
 
 (defn inventory-pool []
-  (if-not @inventory-pool/data*
+  (if-not @core/data*
     [wait-component]
     [:div
      [table/container
@@ -64,20 +38,20 @@
            [:input.custom-control-input {:id "active-switch"
                                          :type "checkbox",
                                          :disabled true,
-                                         :checked (:is_active @inventory-pool/data*)}]
+                                         :checked (:is_active @core/data*)}]
            [:label.custom-control-label {:for "active-switch"}]]]]
         [:tr.name
          [property-td "Name" "name"]
          [:td.name
-          (:name @inventory-pool/data*)]]
+          (:name @core/data*)]]
         [:tr.shortname
          [property-td "Short Name" "shortname"
           "Prefix for auto-generated inventory codes"]
-         [:td.shortname (:shortname @inventory-pool/data*)]]
+         [:td.shortname (:shortname @core/data*)]]
         [:tr.email
          [property-td "Email" "email"
           "from_address for emails send in the name of this pool"]
-         [:td.email (:email @inventory-pool/data*)]]
+         [:td.email (:email @core/data*)]]
         [:tr.description
          [property-td "Description" "description"
           "Visible for customers in the borrow app"]
@@ -85,12 +59,12 @@
           [:div {:style {:white-space "break-spaces",
                          :overflow-y "auto"
                          :height "200px"}}
-           (:description @inventory-pool/data*)]]]
+           (:description @core/data*)]]]
         [:tr.default-contract-note
          [property-td "Default Contract Note" "default_contract_note"]
          [:td.default-contract-note
           {:style {:white-space "break-spaces"}}
-          (:default_contract_note @inventory-pool/data*)]]
+          (:default_contract_note @core/data*)]]
         [:tr.print-contracts
          [property-td "Print Contracts" "print_contracts"
           "Whether to open print dialog automatically upon hand over"]
@@ -99,7 +73,7 @@
            [:input.custom-control-input {:id "print-contracts-switch"
                                          :type "checkbox",
                                          :disabled true,
-                                         :checked (:print_contracts @inventory-pool/data*)}]
+                                         :checked (:print_contracts @core/data*)}]
            [:label.custom-control-label {:for "print-contracts-switch"}]]]]
         [:tr.automatic-suspension
          [property-td "Automatic Suspension" "automatic_suspension"
@@ -109,13 +83,14 @@
            [:input.custom-control-input {:id "automatic-suspension-switch"
                                          :type "checkbox",
                                          :disabled true,
-                                         :checked (:automatic_suspension @inventory-pool/data*)}]
+                                         :checked (:automatic_suspension @core/data*)}]
            [:label.custom-control-label {:for "automatic-suspension-switch"}]]]]
-        [:tr.automatic-suspension-reason
-         [property-td "Automatic Suspension Reason" "automatic_suspension_reason"]
-         [:td.automatic-suspension-reason
-          {:style {:white-space "break-spaces"}}
-          (:automatic_suspension_reason @inventory-pool/data*)]]
+        (when (:automatic_suspension @core/data*)
+          [:tr.automatic-suspension-reason
+           [property-td "Automatic Suspension Reason" "automatic_suspension_reason"]
+           [:td.automatic-suspension-reason
+            {:style {:white-space "break-spaces"}}
+            (:automatic_suspension_reason @core/data*)]])
         [:tr.required-purpose
          [property-td "Hand Over Purpose" "required_purpose"
           "Whether the specification of hand over purpose is required."]
@@ -124,21 +99,21 @@
            [:input.custom-control-input {:id "required-purpose"
                                          :type "checkbox",
                                          :disabled true,
-                                         :checked (:required_purpose @inventory-pool/data*)}]
+                                         :checked (:required_purpose @core/data*)}]
            [:label.custom-control-label {:for "required-purpose"}]]]]]}]
-     [edit-inventory-pool]
-     [delete-inventory-pool]]))
+     [edit/button]
+     [delete/button]]))
 
 (defn page []
   [:<>
    [routing/hidden-state-component
-    {:did-mount inventory-pool/clean-and-fetch}]
-   (if-not @inventory-pool/data*
+    {:did-mount core/clean-and-fetch}]
+   (if-not @core/data*
      [:div.my-5
       [wait-component " Loading Data ..."]]
      [:article.inventory-pool.my-5
       [:h1.my-5
-       [inventory-pool/name-component]]
-      [nav/tabs (join ["/admin/inventory-pools/" @inventory-pool/id*])]
+       [core/name-component]]
+      [nav/tabs (join ["/admin/inventory-pools/" @core/id*])]
       [inventory-pool]
-      [inventory-pool/debug-component]])])
+      [core/debug-component]])])
