aecf9b9a16feb84ead213e72780a77741a5eff95
Jan Pistor <pistor.jan@gmail.com>
2024-06-20 15:36:20 +0200

    wip

--- src/leihs/admin/resources/inventory_pools/inventory_pool/opening_times/main.cljs
+++ src/leihs/admin/resources/inventory_pools/inventory_pool/opening_times/main.cljs
@@ -4,10 +4,11 @@
    [leihs.admin.resources.inventory-pools.inventory-pool.core :as inventory-pool]
    [leihs.admin.resources.inventory-pools.inventory-pool.holidays.core :as holidays-core]
    [leihs.admin.resources.inventory-pools.inventory-pool.holidays.main :as holidays]
-   [leihs.admin.resources.inventory-pools.inventory-pool.nav :as nav]
    [leihs.admin.resources.inventory-pools.inventory-pool.workdays.core :as workdays-core]
    [leihs.admin.resources.inventory-pools.inventory-pool.workdays.main :as workdays]
-   [leihs.admin.state :as state]))
+   [leihs.admin.state :as state]
+   [leihs.admin.utils.misc :refer [wait-component]]
+   [leihs.core.routing.front :as routing]))
 
 (defn debug-component []
   (when (:debug @state/global-state*)
@@ -25,10 +26,15 @@
 
 (defn page []
   [:article.inventory-pool-opening-times
-   [inventory-pool/header]
-   [inventory-pool/tabs]
-   [:article.m-0.mb-5.container
-    [:div.row
-     [:div.col-6 [workdays/component]]
-     [:div.col-6 [holidays/component]]]]
-   [debug-component]])
+   [routing/hidden-state-component
+    {:did-mount inventory-pool/clean-and-fetch}]
+
+   (if-not @inventory-pool/data*
+     [wait-component]
+     [:<>
+      [inventory-pool/header]
+      [inventory-pool/tabs]
+      [:div.row
+       [:div.col-6 [workdays/component]]
+       [:div.col-6 [holidays/component]]]
+      [debug-component]])])
