3ba607a19584ab59173381500787be5ca6553f40
Jan Pistor <pistor.jan@gmail.com>
2024-06-20 17:22:36 +0200

    wip

--- src/leihs/admin/resources/groups/group/main.cljs
+++ src/leihs/admin/resources/groups/group/main.cljs
@@ -2,7 +2,6 @@
   (:require
    [leihs.admin.common.components.navigation.breadcrumbs :as breadcrumbs]
    [leihs.admin.common.components.table :as table]
-   [leihs.admin.paths :as paths :refer [path]]
    [leihs.admin.resources.groups.group.core :refer [clean-and-fetch data*
                                                     debug-component]]
    [leihs.admin.resources.groups.group.delete :as delete]
@@ -12,49 +11,7 @@
    [leihs.admin.utils.misc :refer [wait-component]]
    [leihs.core.auth.core :as auth]
    [leihs.core.routing.front :as routing]
-   [react-bootstrap :as react-bootstrap :refer [Button Nav]]
-   [reagent.core :as reagent]))
-
-(defn some-lending-manager-and-group-unprotected? [current-user-state _]
-  (and (pool-auth/some-lending-manager? current-user-state _)
-       (boolean  @data*)
-       (and (-> @data* :admin_protected not)
-            (-> @data* :system_admin_protected not))))
-
-(defn admin-and-group-not-system-admin-protected?
-  [current-user routing-state]
-  (and (auth/admin-scopes? current-user routing-state)
-       (-> @data* :system_admin_protected not)))
-
-(defn edit-button []
-  (let [show (reagent/atom false)]
-    (fn []
-      (when (auth/allowed?
-             [admin-and-group-not-system-admin-protected?
-              auth/system-admin-scopes?
-              some-lending-manager-and-group-unprotected?])
-        [:<>
-         [:> Button
-          {:onClick #(reset! show true)}
-          "Edit"]
-         [edit/dialog {:show @show
-                       :onHide #(reset! show false)}]]))))
-
-(defn delete-button []
-  (let [show (reagent/atom false)]
-    (fn []
-      (when (auth/allowed?
-             [admin-and-group-not-system-admin-protected?
-              auth/system-admin-scopes?
-              some-lending-manager-and-group-unprotected?])
-        [:<>
-         [:> Button
-          {:variant "danger"
-           :className "ml-3"
-           :onClick #(reset! show true)}
-          "Delete"]
-         [delete/dialog  {:show @show
-                          :onHide #(reset! show false)}]]))))
+   [react-bootstrap :as react-bootstrap :refer [Nav]]))
 
 (defn properties-table []
   (let [data @data*]
@@ -95,35 +52,36 @@
        [:h1.mt-3 name]])))
 
 (defn page []
-  (if (empty? @data*)
-    [:div.mt-5
-     [wait-component]
-     [routing/hidden-state-component
-      {:did-mount clean-and-fetch
-       :did-change clean-and-fetch}]]
-    [:article.group
-     [header]
+  [:article.group
+   [routing/hidden-state-component
+    {:did-change clean-and-fetch}]
 
-     [:section
-      [properties-table]
-      [edit-button]
-      [delete-button]]
+   (if (empty? @data*)
+     [:div.mt-5
+      [wait-component]]
+     [:<>
+      [header]
 
-     [:section
-      [:> Nav {:variant "tabs" :className "mt-5"
-               :defaultActiveKey "users"}
-       [:> Nav.Item
-        [:> Nav.Link {:active true} "Inventory Pools"]]
+      [:section
+       [properties-table]
+       [edit/button]
+       [delete/button]]
 
-       (when (auth/allowed?
-              [auth/admin-scopes?
-               pool-auth/some-lending-manager?])
-         [:> Nav.Item
-          [:> Nav.Link
-           {:href (-> (:path @routing/state*)
-                      (clojure.core/str "/users/"))}
-           "Users"]])]
+      [:section
+       [:> Nav {:variant "tabs" :className "mt-5"
+                :defaultActiveKey "users"}
+        [:> Nav.Item
+         [:> Nav.Link {:active true} "Inventory Pools"]]
 
-      [:div
-       [inventory-pools/table-component]]
-      [debug-component]]]))
+        (when (auth/allowed?
+               [auth/admin-scopes?
+                pool-auth/some-lending-manager?])
+          [:> Nav.Item
+           [:> Nav.Link
+            {:href (-> (:path @routing/state*)
+                       (clojure.core/str "/users/"))}
+            "Users"]])]
+
+       [:div
+        [inventory-pools/table-component]]
+       [debug-component]]])])
