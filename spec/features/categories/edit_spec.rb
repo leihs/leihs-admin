21d00155d9d5db4457ef8a014bf0a380fec95b0e
Jan Pistor <pistor.jan@gmail.com>
2024-12-18 12:54:50 +0100

    refactor: edit spec

--- spec/features/categories/edit_spec.rb
+++ spec/features/categories/edit_spec.rb
@@ -8,6 +8,7 @@ feature "Manage categories ", type: :feature do
     @parent_category = FactoryBot.create(:model_group, name: "Parent Category")
     child_category = FactoryBot.create(:model_group, name: "Child Category")
     nthchild_category = FactoryBot.create(:model_group, name: "Nth Category")
+    @another_category = FactoryBot.create(:model_group, name: "Another Category")
 
     FactoryBot.create(:model_group_link, parent: @parent_category, child: child_category)
     FactoryBot.create(:model_group_link, parent: child_category, child: nthchild_category)
@@ -24,10 +25,9 @@ feature "Manage categories ", type: :feature do
       within("aside nav") do
         click_on "Categories"
       end
-      binding.pry
 
-      click_on @parent_category.name
-      expect(page).to have_content @parent_category.name
+      click_on @another_category.name
+      expect(page).to have_content @another_category.name
       @category_path = current_path
 
       click_on "Edit"
@@ -45,18 +45,30 @@ feature "Manage categories ", type: :feature do
         fill_in "label", with: label
       end
 
+      attach_file("user-image", "./spec/data/anon.jpg")
       click_on "Save"
 
+      expect(page).to have_content("Parent Category <- Child Category <- #{name}")
       expect(page).to have_content name
       expect(page).to have_content label
+      within ".category" do
+        expect(page).to have_css("img[src]")
+      end
 
       within("aside nav") do
         click_on "Categories"
       end
 
+      click_on "reset-tree"
+      expect(page).not_to have_content label
+      expect(page).not_to have_content @another_category.name
+
+      find('input[aria-label="Category search field"]').set(name)
+      find('input[aria-label="Category search field"]').send_keys(:enter)
+
       all("li", text: "Parent Category").last.click
       all("li", text: "Child Category").last.click
-      expect(page).to have_content name
+      expect(page).to have_content label
     end
   end
 end
